bbb3a5cee3625c263d9af763bc8ac69d418299ba
Steven Dorling <sjdorling@gmail.com>
2022-08-01 20:22:25 +0100

    Add project files.

---
+++ ICache.cs
@@ -0,0 +1,11 @@
+﻿namespace Cache
+{
+    public interface ICache
+    {
+        // return the value if key exists in cache, or -1 if not. 
+        int GetCachedValueByKey(int key);
+
+        // add a new value to the cache
+        void AddValueToCacheByKey(int key, int value);
+    }
+}

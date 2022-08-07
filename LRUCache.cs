38e05589bccfc570f1d5d833d747cfb923907546
Steven Dorling <sjdorling@gmail.com>
2022-08-07 18:05:33 +0100

    All unit tests pass. My own implementation. Doesn't meet extra requirements. Suspect there will be a limit on the capacity because using int datatype.

--- LRUCache.cs
+++ LRUCache.cs
@@ -1,93 +1,87 @@
-﻿using Cache.LRU;
-using System.Collections.Generic;
+﻿using Microsoft.Extensions.Caching.Memory;
 
 namespace Cache
 {
 	public sealed class LRUCache : ICache
 	{
-		private readonly object cacheLock = new object();
-		private int capacity;
-		private int count;
-		Dictionary<int, LRUNode> map;
-		LRUDoubleLinkedList doubleLinkedList;
-		public LRUCache(int capacity)
-		{
-			this.capacity = capacity;
-			count = 0;
-			map = new Dictionary<int, LRUNode>();
-			doubleLinkedList = new LRUDoubleLinkedList();
+
+		static MemoryCacheOptions cacheOption = new MemoryCacheOptions { };
+		static MemoryCacheEntryOptions cacheEntryOption = new MemoryCacheEntryOptions { };
+		static MemoryCache cache = new MemoryCache(cacheOption);
+		public Dictionary<int, int> _cacheLedger = new Dictionary<int, int>();
+		private int _capacity = 0;
+		private int counter = 0;
+		
+		public LRUCache(int capacity, int size)
+        {
+			cacheOption.SizeLimit = capacity;
+			cacheEntryOption.Size = size;
+			this._capacity = capacity;
+			
+		}
+
+		public void EmptyTheCache()
+        {
+			List<int> cacheKeys = _cacheLedger.Select(c => c.Key).ToList();
+			foreach (int cacheKey in cacheKeys)
+			{
+				cache.Remove(cacheKey);
+			}
 		}
 
 		public object GetCachedValueByKey(int key)
 		{
-			lock (cacheLock)
-			{
+			int counter = _cacheLedger[key];
+			object existingCachedValue;
 
-				if (!map.ContainsKey(key)) return -1;
-				LRUNode node = map[key];
-				doubleLinkedList.RemoveNode(node);
-				// each time when access the node, we move it to the top
-				doubleLinkedList.AddNodeToTop(node);
-				return node.Value;
+			// If the key exists in the cache
+			if (cache.TryGetValue(key, out existingCachedValue))
+            {
+				// then update the ledger then return the corresponding value
+				_cacheLedger[key] = counter++;
+				return existingCachedValue;	
+            }
 
-			}
+			return null;
 		}
 
-		public void AddValueToCacheByKey(int key, object value, out string? rtrnMessage)
+		public void AddValueToCacheByKey(int key, object value)
 		{
-			// If key/value pair already exist..
-			if (map.ContainsKey(key))
-			{
-				object soonToBeOldValue = GetCachedValueByKey(key);
+			// If the ledger isn't full
+			if (_cacheLedger.Count < _capacity)
+            {
+				// then insert the new key/value pair into the memory cache, or overwrite it if it already exsits
+				cache.Set(key, value, cacheEntryOption);
 
-				lock (cacheLock)
+				// then update the ledger
+				if (_cacheLedger.ContainsKey(key))
                 {
-					// ..just need to update value and move it to the top
-					LRUNode node = map[key];
-					doubleLinkedList.RemoveNode(node);
-					node.Value = value;
-					doubleLinkedList.AddNodeToTop(node);
-
+					//int counter = _cacheLedger[key];
+					_cacheLedger[key] = counter++;
 				}
-
-				rtrnMessage = $"Value {soonToBeOldValue.ToString} has been removed from the cache";
-
-				//return $"Value {soonToBeOldValue.ToString} has been removed from the cache";
+				else
+                {
+					_cacheLedger[key] = counter++;
+                }
 			}
 			else
-			{
-				// if cache is full.. 
-				if (count == capacity)
-				{
-					
-					lock (cacheLock)
-                    {
-						// ..then remove the least recently used node
-						LRUNode lru = doubleLinkedList.RemoveLRUNode();
-						map.Remove(lru.Key);
-						count--;
+            {
+				// work out which is the least used, which is the one with the lowest counter
+				var leastUsed = _cacheLedger.OrderBy(c => c.Value).First().Key;
 
-						rtrnMessage = "";
+				// then remove the least used one from the cache
+				cache.Remove(leastUsed);
 
-					}
-					
-				}
+				// and replace it with the new one
+				cache.Set(key, value, cacheEntryOption);
 
-                lock (cacheLock)
-                {
-					// Otherwise, add a new node
-					LRUNode node = new LRUNode(key, value);
-					doubleLinkedList.AddNodeToTop(node);
-					map[key] = node;
-					count++;
+				// and then remove the ledger entry
+				_cacheLedger.Remove(leastUsed);
 
-					rtrnMessage = "";
+				// and replace it with a new one
+				_cacheLedger.Add(key, counter++);
 
-				}
-				
 			}
-
 		}
-
 	}
 }
